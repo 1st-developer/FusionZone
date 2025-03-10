f11b8ff7f26f4c5e64e193f33d26977a633dd4d2
unknown <mustafasmart671st@gmail.com>
2025-03-10 15:16:40 +0300

    chnge

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "http://localhost:2019/api";
+export const BASE_API_URL = "http://localhost:2219/api";
 export const DEFAULT_ERROR_MESSAGE = "Oops!! Something went wrong!, Pleace try again leter"
